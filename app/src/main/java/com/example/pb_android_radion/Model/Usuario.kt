3a0c1ac894c2aeb32ca2daee7ba8ac4f3f48e24a
anathsu <willian.silva@al.infnet.edu.br>
2020-03-13 22:53:55 -0300

    Adicionado EsqueciSenhaFragment Adicionado Esqueci senha na tela de Login

--- app/src/main/java/com/example/pb_android_radion/Model/Usuario.kt
+++ app/src/main/java/com/example/pb_android_radion/Model/Usuario.kt
@@ -1,5 +1,13 @@
 package com.example.pb_android_radion.Model
 
 class Usuario(email: String, senha: String) {
+    lateinit var nome: String
+    lateinit var sobrenome: String
+    lateinit var cpf: String
+    lateinit var apelido: String
+    lateinit var estado: String
+    lateinit var telefone: String
+    lateinit var ddd: String
+
 
 }
