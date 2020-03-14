283b699ae4c6a3065c71aba11f1461297b51b4d7
anathsu <willian.silva@al.infnet.edu.br>
2020-03-13 23:33:55 -0300

    Adicionado Cadastro compelto no fragment de cadastro

--- app/src/main/java/com/example/pb_android_radion/Fragment/LoginFragment.kt
+++ app/src/main/java/com/example/pb_android_radion/Fragment/LoginFragment.kt
@@ -25,7 +25,7 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        textoCadastro.setOnClickListener {
+        textoCadastroLogin.setOnClickListener {
             findNavController().navigate(R.id.cadastroFragment)
         }
     }
