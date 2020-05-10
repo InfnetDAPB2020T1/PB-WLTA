a6948032ab507988fcb44e6d8ee5d389201a8bf9
ThamyresAbal <thamyresabal@gmail.com>
2020-05-10 19:39:55 -0300

    Exibindo as informacoes no perfil

--- app/src/main/java/com/example/pb_android_radion/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/pb_android_radion/fragment/LoginFragment.kt
@@ -3,6 +3,7 @@ package com.example.pb_android_radion.fragment
 import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,6 +17,7 @@ import com.example.pb_android_radion.R
 import com.example.pb_android_radion.service.AppDatabaseService
 import com.example.pb_android_radion.viewModel.UsuarioViewModel
 import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.layout_cadastro.*
 
 class LoginFragment : Fragment() {
 
@@ -83,18 +85,42 @@ class LoginFragment : Fragment() {
             super.onPostExecute(result)
 
             result!!.forEach {
+
                 if(boxEmailLogin.text.toString() == it.email &&
                     boxSenhaLogin.text.toString() == it.senha){
-                    usuarioViewModel.usuarioLogado = it
-                    /*Log.i("Usuario", "${usuarioViewModel.usuarioLogado!!.email}," +
-                            "${usuarioViewModel.usuarioLogado!!.senha}," +
-                            "${usuarioViewModel.usuarioLogado!!.apelido}," +
-                            "${usuarioViewModel.usuarioLogado!!.nome}," +
-                            "${usuarioViewModel.usuarioLogado!!.sobrenome}," +
-                            "${usuarioViewModel.usuarioLogado!!.cpf}," +
-                            "${usuarioViewModel.usuarioLogado!!.estado}," +
-                            "${usuarioViewModel.usuarioLogado!!.ddd}," +
-                            "${usuarioViewModel.usuarioLogado!!.telefone}")*/
+
+                    //preenchendo o objeto usuarioLogado
+                    usuarioViewModel.usuarioLogado = Usuario(
+                        apelido = it.apelido,
+                        imagem = it.imagem,
+                        email = it.email,
+                        senha = it.senha,
+                        nome = it.nome,
+                        cpf = it.cpf,
+                        estado = it.estado,
+                        ddd = it.ddd,
+                        telefone = it.telefone
+                    )
+                /*    usuarioViewModel.usuarioLogado!!.nome = it.nome
+                    usuarioViewModel.usuarioLogado!!.apelido = it.apelido
+                    usuarioViewModel.usuarioLogado!!.email = it.email
+                    usuarioViewModel.usuarioLogado!!.senha = it.senha
+                    usuarioViewModel.usuarioLogado!!.cpf = it.cpf
+                    usuarioViewModel.usuarioLogado!!.telefone = it.telefone
+                    usuarioViewModel.usuarioLogado!!.ddd = it.ddd
+                    usuarioViewModel.usuarioLogado!!.estado = it.estado*/
+
+                    //usuarioViewModel.usuarioLogado = it --> salva o endereço de memoria do objeto
+
+                    /*Log.i("Usuario", "${it.email}," +
+                            "${it.senha}," +
+                            "${it.apelido}," +
+                            "${it.nome}," +
+                            "${it.cpf}," +
+                            "${it.estado}," +
+                            "${it.ddd}," +
+                            "${it.telefone}")*/
+
                     startActivity(Intent(activity?.baseContext, MainActivity::class.java))
                 }else{
                     Toast.makeText(activity!!.baseContext, "Usuário inválido",
--- app/src/main/java/com/example/pb_android_radion/fragment/PerfilFragment.kt
+++ app/src/main/java/com/example/pb_android_radion/fragment/PerfilFragment.kt
@@ -1,14 +1,23 @@
 package com.example.pb_android_radion.fragment
 
+import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
+import com.example.pb_android_radion.MainActivity
 import com.example.pb_android_radion.R
+import com.example.pb_android_radion.model.Usuario
+import com.example.pb_android_radion.service.AppDatabaseService
 import com.example.pb_android_radion.viewModel.UsuarioViewModel
+import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_perfil.*
+import kotlinx.android.synthetic.main.fragment_perfil.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -29,8 +38,39 @@ class PerfilFragment : Fragment() {
             usuarioViewModel = ViewModelProviders.of(it).get(UsuarioViewModel::class.java)
         }
 
-      //  textViewApelidoPerfil.text = usuarioViewModel.usuarioLogado!!.apelido
-       // textViewNomeSobrenomePerfil.text = usuarioViewModel.usuarioLogado!!.nome
+        OperacaoBancoTask().execute()
+        //textViewApelidoPerfil.text = usuarioViewModel.usuario!!.apelido
+        // textViewNomeSobrenomePerfil.text = usuarioViewModel.usuario!!.nome
+    }
 
+    inner class OperacaoBancoTask : AsyncTask<Unit, Unit, Array<Usuario>>() {
+
+        override fun doInBackground(vararg params: Unit?): Array<Usuario> {
+            var db = AppDatabaseService.getInstance(activity!!.baseContext)
+
+            return db.usuarioDao().listarUsuarios()
+        }
+
+        override fun onPostExecute(result: Array<Usuario>?) {
+            super.onPostExecute(result)
+
+          /*  Log.i("Usuario", "${usuarioViewModel.usuarioLogado!!.nome}," +
+                           "${usuarioViewModel.usuarioLogado!!.senha}," +
+                           "${usuarioViewModel.usuarioLogado!!.apelido}," +
+                           "${usuarioViewModel.usuarioLogado!!.nome}," +
+                           "${usuarioViewModel.usuarioLogado!!.cpf}," +
+                           "${usuarioViewModel.usuarioLogado!!.estado}," +
+                           "${usuarioViewModel.usuarioLogado!!.ddd}," +
+                           "${usuarioViewModel.usuarioLogado!!.telefone}")*/
+
+            textViewNomeSobrenomePerfil.text = "//usuarioViewModel.usuarioLogado!!.nome"
+            textViewApelidoPerfil.text = "Arlequina"
+            textViewCPFUsuario.text = "Harley Queen"
+            textViewEmailUsuario.text = "Harley Queen"
+            textViewTelefoneUsuario.text = "Harley Queen"
+            textViewEstadoUsuario.text = "Harley Queen"
+            // Botão drawable para trocar senha chamando um card de dialogo
+            TextViewSair// sair da aplicação
+        }
     }
 }
